2b5a7cf8b035c7b7b34ae321128a009242091a57
JasperDre <jasperdre@gmail.com>
2018-09-24 20:37:45 +0200

    Added template project

---
+++ Template/src/main/java/gloryrock/template/main/Template.java
@@ -0,0 +1,20 @@
+package gloryrock.template.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Template extends JavaPlugin
+{
+	// Fired when plugin is first enabled
+    @Override
+    public void onEnable()
+    {
+    	
+    }
+    
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable()
+    {
+    	
+    }
+}
